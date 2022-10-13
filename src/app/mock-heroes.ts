dfedd017e96a87db9d748388ae3e9f0aeaad416b
Merauta Daniel <merautadaniel@gmail.com>
2022-10-13 11:22:49 +0100

    initial commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Ashe' },
+  { id: 13, name: 'Janna' },
+  { id: 14, name: 'Braum' },
+  { id: 15, name: 'Draven' },
+  { id: 16, name: 'Vayne' },
+  { id: 17, name: 'Morgana' },
+];
